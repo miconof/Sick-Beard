cc0f214b8e6aeafb7dc39a280d96041dac07a8d1
miconof <miconof@gmail.com>
2012-11-24 10:29:21 +0100

    Modify install script to setup periscope

--- install.sh
+++ install.sh
@@ -2,7 +2,15 @@
 
 cd `dirname $0`
 
+echo "Copying config files..."
 cp init.ubuntu /etc/init.d/sickbeard
 cp defaults /etc/default/sickbeard
+
+echo "Installing periscope..."
+apt-get -y install python-setuptools python-beautifulsoup
+cd .. && su pi -c 'git clone https://github.com/patrickdessalle/periscope'
+cd periscope && python setup.py install
+
+echo "Start Sickbeard service and add it to the default runlevel..."
 /etc/init.d/sickbeard start
 update-rc.d sickbeard defaults
